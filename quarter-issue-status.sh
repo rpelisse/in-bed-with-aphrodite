75550ed4328315c439f25db85a59d83f795206f3
Romain Pelisse <belaran@gmail.com>
2017-05-04 15:14:46 +0200

    misc: - replaced MODIFIED by RESOLVED just to be consistent with JIRA naming - renamed var properly

--- quarter-issue-status.sh
+++ quarter-issue-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 readonly BURN_RATE_FILE="${HOME}/Documents/redhat/jboss-set/reports/cw-$(weekno | cut -f2 -d: | sed -e 's/ //g').$(date +%Y).md"
 
-readonly NO_FILTER=${1}
+readonly FILTER_RESOLVED_ISSUES=${1}
 
 if [ ! -e "${BURN_RATE_FILE}" ]; then
   echo "Burn rate file does not exist: ${BURN_RATE_FILE}"
@@ -19,9 +19,9 @@ output=$(mktemp)
 $(pwd)/issue-status.sh ${URLS} 2> /dev/null > "${output}"
 nbIssue=$(wc -l "${output}")
 echo "Burn Rate is : ${nbIssue}"
-if [ ! -z "${NO_FILTER}" ]; then
+if [ ! -z "${FILTER_RESOLVED_ISSUES}" ]; then
     cat "${output}" | sed -e '/POST/d' -e '/VERIFIED/d' -e '/CLOSED/d'  -e '/MODIFIED/d' | sort -k2
 else
-    cat "${output}"
+    cat "${output}" | sort -k2 | sed -e 's/MODIFIED/RESOLVED/'
 fi
 rm -f "${output}"
