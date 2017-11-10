5e238ace213f8c3866a319bb49e62b4b25a1a820
Romain Pelisse <belaran@gmail.com>
2017-11-10 12:26:10 +0100

    Rework run-scala to use bugclerk-dist instead of Aphrodite Aphrodite does no longer provides a shaded jar, so using the bugclerk-dist instead

--- run-scala.sh
+++ run-scala.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-
-readonly JCOMMANDER_VERSION='1.48'
-readonly JCOMMANDER=${JCOMMANDER:-"${HOME}/.m2/repository/com/beust/jcommander/${JCOMMANDER_VERSION}/jcommander-${JCOMMANDER_VERSION}.jar"}
-
-readonly APHRODITE_VERSION=${APHRODITE_VERSION:-'0.4.2-SNAPSHOT'}
-readonly APHRODITE=${APHRODITE:-"${HOME}/.m2/repository/org/jboss/set/aphrodite/${APHRODITE_VERSION}/aphrodite-${APHRODITE_VERSION}.jar"}
-
+readonly CLASSPATH=${CLASSPATH:-"${HOME}/.m2/repository/org/jboss/jbossset/bugclerk-dist/1.0.1.Final-SNAPSHOT/bugclerk-dist-1.0.1.Final-SNAPSHOT-shaded.jar"}
 readonly APHRODITE_CONFIG=${APHRODITE_CONFIG:-"$(pwd)/aphrodite-config.json"}
-
-readonly INTERACTIVE=${INTERACTIVE:-''}
 
 readonly SCRIPT=${1}
 
 shift
 
-scala -Daphrodite.config=${APHRODITE_CONFIG}  -classpath "${APHRODITE}:${JCOMMANDER}" "${SCRIPT}" ${@}  2>&1 | \
-  sed -e '/INFOS:/d' -e  '/SLF4J:/d' -e '/logWarnMessage/d' -e '/svn repository/d' -e '/No repositories found which correspond to url:/d' -e '/org.jboss.set.aphrodite.Aphrodite init/d'
+scala  -Daphrodite.config=${APHRODITE_CONFIG}  -classpath "${CLASSPATH}" "${SCRIPT}" ${@}  2>&1 | \
+    sed -e '/INFOS:/d' -e  '/SLF4J:/d' -e '/logWarnMessage/d' -e '/svn repository/d' \
+    -e '/No repositories found which correspond to url:/d' -e '/org.jboss.set.aphrodite.Aphrodite init/d' \
+    -e '/Unable to initiatilise Aphrodite, as a valid org.jboss.set.aphrodite.spi.RepositoryService does not exist./d'
