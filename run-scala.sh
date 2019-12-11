99fa5b40fafcb79f1ff677b9483d2ddd2b1b1348
Romain Pelisse <belaran@gmail.com>
2019-12-11 15:59:27 +0100

    Misc. updates to run-scala.sh

--- run-scala.sh
+++ run-scala.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-readonly BUGCLERK_JAR=${BUGCLERK_JAR:-"${HOME}/.m2/repository/org/jboss/jbossset/bugclerk-dist/1.0.2/bugclerk-dist-1.0.2-shaded.jar"}
-readonly JSOUP_JAR=${JSOUP_JAR:-'/home/rpelisse/.m2/repository/org/jsoup/jsoup/1.8.3.redhat-2/jsoup-1.8.3.redhat-2.jar'}
+readonly BUGCLERK_JAR=${BUGCLERK_JAR:-"${HOME}/.m2/repository/org/jboss/jbossset/bugclerk-dist/1.0.6.Final-SNAPSHOT/bugclerk-dist-1.0.6.Final-SNAPSHOT-shaded.jar"}
+readonly JSOUP_JAR=${JSOUP_JAR:-'/home/rpelisse/.m2/repository/org/jsoup/jsoup/1.12.1/jsoup-1.12.1.jar'}
 readonly CLASSPATH=${BUGCLERK_JAR}:${JSOUP_JAR}:${CLASSPATH}
 
 readonly APHRODITE_CONFIG=${APHRODITE_CONFIG:-"$(pwd)/aphrodite-config.json"}
@@ -26,4 +26,5 @@ shift
 scala  -Daphrodite.config=${APHRODITE_CONFIG}  -classpath "${CLASSPATH}" "${SCRIPT}" ${@}  2>&1 | \
     sed -e '/INFOS:/d' -e  '/SLF4J:/d' -e '/logWarnMessage/d' -e '/svn repository/d' \
     -e '/No repositories found which correspond to url:/d' -e '/org.jboss.set.aphrodite.Aphrodite init/d' \
-    -e '/Unable to initiatilise Aphrodite, as a valid org.jboss.set.aphrodite.spi.RepositoryService does not exist./d'
+    -e '/Unable to initiatilise Aphrodite, as a valid org.jboss.set.aphrodite.spi.RepositoryService does not exist./d' \
+    -e '/INFO: Aphrodite Initialisation Complete/d' -e '/INFO: Initiating Aphrodite .../d'
